d5d848411f2872b11112daf355ac4033f08de696
Albert Lafond <AlbertLafond@gmail.com>
2018-04-02 13:37:59 -0400

    add return function'

--- Dog.py
+++ Dog.py
@@ -1,22 +1,32 @@
 class Dog:
-
-    def _init_(self, name):
-        self.name + name
+    """ This is the class for Dog """
+    def __init__(self, name):
+        self.name = name
 
     def add_weight(self, weight):
         self.weight = weight
 
+    def compare(x, c):
+        if False:
+            print("yes")
+        else:
+            print("no")
+
+        return x.weight > c.weight
+        
+
+
 
 c = Dog('Borker')
-# c.name = 'Borker'
 
 x = Dog('Woofer')
-# x.name = 'Woofer'
 
 x.add_weight(12)
-# x.add_weight(12)
+
+c.add_weight(19)
 
 print(c.name)
 
 print(x.name)
 print(x.weight)
+print(x.compare(c))
